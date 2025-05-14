0ca1443b49dc02357474ccef19fcb2805f3ec562
sachintha990 <sachintharathnayaka12@gmail.com>
2025-05-14 09:52:54 +0530

    Initialize Hive and register adapters in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:meditator/models/meditation_model.dart';
+import 'package:meditator/models/mindfull_exercise_model.dart';
+import 'package:meditator/models/sleep_content_model.dart';
+import 'package:meditator/router/router.dart';
+
+import 'package:provider/provider.dart';
 import 'package:meditator/providers/filter_provider.dart';
 import 'package:meditator/providers/meditation_provider.dart';
 import 'package:meditator/providers/mindfull_exercise_provider.dart';
 import 'package:meditator/providers/sleep_content_provider.dart';
-import 'package:meditator/router/router.dart';
-import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  //register the adapters
+  Hive.registerAdapter(MeditationContentAdapter());
+  Hive.registerAdapter(MindfulnessExerciseAdapter());
+  Hive.registerAdapter(SleepContentAdapter());
+
+  // Open Hive box
+  await Hive.openBox('meditations_data');
+  await Hive.openBox('mindfull_exercises');
+  await Hive.openBox('sleep_contents');
   runApp(
     MultiProvider(
       providers: [
